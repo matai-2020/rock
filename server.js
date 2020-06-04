f1207fa030065b64f46bb8281699b0d24cf03d56
Jake Hurley <63322006+jake-hurley@users.noreply.github.com>
2020-06-05 10:30:14 +1200

    add footer/header partials

--- server.js
+++ server.js
@@ -2,18 +2,29 @@ const path = require('path')
 const express = require('express')
 const hbs = require('express-handlebars')
 const routes = require('./routes')
+const fs = require('fs')
 
-const server =  express()
+const server = express()
 module.exports = server
 
-server.engine('hbs', hbs({extname: 'hbs'}))
+server.engine('hbs', hbs({ extname: 'hbs' }))
 server.set('view engine')
 
-// //server.use(express.urlencoded({
-//     extended: false
-// }))
+server.use(express.urlencoded({
+  extended: false
+}))
 
-//const staticFolder = path.join(__dirname, 'public')
-//server.use(express.static(staticFolder))
+const staticFolder = path.join(__dirname, 'public')
+server.use(express.static(staticFolder))
 
 server.use('/', routes)
+
+server.get('/question/:id', (req, res) => {
+  let id = req.params.id
+  //readfile
+  //link data,json
+  
+  //check if answer is === a rock or the rock
+  //render right/id
+  //render wrong/id
+})
