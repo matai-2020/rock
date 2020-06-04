df4bc08c79ee3d221061d7646190154f04e9130d
Jake Hurley <63322006+jake-hurley@users.noreply.github.com>
2020-06-05 11:13:08 +1200

    add onclick functions to question page

--- routes.js
+++ routes.js
@@ -1,8 +1,44 @@
 const express = require('express')
-const router = express.Router() 
+const router = express.Router()
+const path = require('path')
+const filePath = path.join(__dirname, 'data.json')
+const fs = require('fs')
 
 module.exports = router
 
 router.get('/', (req, res) => {
-    res.send('hello world')
+  res.send('hello world')
 })
+
+router.get('/question/:id', (req, res) => {
+  let id = req.params.id
+
+  // ONCE BASIC FUNCTIONALITY WORKS ADD RANDOM ID  const randomId = setRandom()
+  // readfile
+
+  fs.readFile(filePath, (err, contents) => {
+    let choice = setChoice(strArg)
+    if (err) return res.send(500)
+    let rockData = JSON.parse(contents)
+    if (rockData.questions[id - 1].answer === choice) {
+      res.render('/right/:id', rockData)
+    } else {
+      res.render('/wrong/:id', rockData)
+    }
+  })
+
+  // link data,json
+
+  // check if answer is === a rock or the rock
+  // render right/id
+  // render wrong/id
+})
+
+function setChoice (straArg) {
+  console.log('click passed')
+  return strArg
+}
+// function setRandom () {
+//   let result = Math.floor(Math.random() * Math.floor(5))
+//   return result
+// }
--- server.js
+++ server.js
@@ -20,20 +20,3 @@ server.use(express.static(staticFolder))
 
 server.use('/', routes)
 
-server.get('/question/:id', (req, res) => {
-  let id = req.params.id
-  let choice = 
-  const randomId = setRandom()
-  // readfile
-  fs.readFile(filePath, (err, contents) => {
-    if (err) return res.send(500)
-    let rockData = JSON.parse(contents)
-    if (questions[id-1].answer === choice
-    
-  })
-  // link data,json
-
-  // check if answer is === a rock or the rock
-  // render right/id
-  // render wrong/id
-})
