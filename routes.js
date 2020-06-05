918d4b385ec20b3e4f1992458436a0a279f894de
Rikke Simone <33711718+RikkeSimone@users.noreply.github.com>
2020-06-05 14:29:30 +1200

    update question with a tags instead of buttons

--- routes.js
+++ routes.js
@@ -4,6 +4,7 @@ const path = require('path')
 const filePath = path.join(__dirname, 'data.json')
 const fs = require('fs')
 
+
 module.exports = router
 
 router.get('/', (req, res) => {
@@ -11,34 +12,19 @@ router.get('/', (req, res) => {
 })
 
 router.get('/question/:id', (req, res) => {
-  let id = req.params.id
-
   // ONCE BASIC FUNCTIONALITY WORKS ADD RANDOM ID  const randomId = setRandom()
   // readfile
-
   fs.readFile(filePath, (err, contents) => {
-    // let choice = setChoice()
+    let id = req.params.id
+    //   // let choice = setChoice()
     if (err) return res.send(500)
     let rockData = JSON.parse(contents)
-    if (rockData.questions[id - 1].answer === setChoice()) {
-      res.render('/right/:id', rockData)
-    } else {
-      res.render('/wrong/:id', rockData)
-    }
+    let { question, answer, fact1, fact0, image0, image1 } = req.body
+    let rock = { question, answer, fact1, fact0, image0, image1 }
+    res.render('question', rockData.questions[id - 1])
   })
-
-  // link data,json
-
-  // check if answer is === a rock or the rock
-  // render right/id
-  // render wrong/id
 })
 
-function setChoice (strArg) {
-  console.log('click passed')
-  return strArg
-}
-// function setRandom () {
-//   let result = Math.floor(Math.random() * Math.floor(5))
-//   return result
+
+
 // }
